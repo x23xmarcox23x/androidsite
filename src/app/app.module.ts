7ea8e619bae16d2bcbcc96ada5f9dedd8976c3ef
Marco Chinaglia <x23xmarcox23x@gmail.com>
2019-05-14 14:21:38 +0200

    fix appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
